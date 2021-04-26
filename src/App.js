64de8e8e178c37dae08c324043c8157453d8c65f
mihirbala <mkbala42@gmail.com>
2021-04-25 23:54:51 -0400

    Basic working code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Map from './components/Map' // import the map here
 
-export default App;
+import './App.css'
+
+
+const App = () => (
+  <div className="App">
+    <Map/>
+  </div>
+)
+
+export default App
